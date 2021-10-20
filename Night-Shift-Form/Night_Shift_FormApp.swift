9abbe920b4e27fd65a52a73003099117f2410d09
Aysel Abdullayeva <aysel2920@gmail.com>
2021-10-21 00:59:12 +0200

    Initial Commit

---
+++ Night-Shift-Form/Night_Shift_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Night_Shift_FormApp.swift
+//  Night-Shift-Form
+//
+//  Created by icelkas on 21/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Night_Shift_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
